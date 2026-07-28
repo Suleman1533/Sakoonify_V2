ec24be362912b1838113ce313cb5c30a8ee7ae97
Suleman <muhammadsuleman1533@gmail.com>
2026-07-28 16:19:03 +0500

    added/ learn lambda funciton with sorted funciton

--- day3_drill.py
+++ day3_drill.py
@@ -1,4 +1,6 @@
 def add(*args):
+    for i in args:
+        print(i)
     return sum(args)
 
 print(add(1,2,3,4))
@@ -9,3 +11,4 @@ lst = [("suleman", 23), ("Rayan", 21), ("Saifullah" , 22)]
 sorted_list = sorted(lst, key = lambda lst : lst[1])
 
 print(sorted_list)
+print(lst)
