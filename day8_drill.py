e7598ec15ecd45d0e193680d87924244aa0bf535
Suleman <muhammadsuleman1533@gmail.com>
2026-08-03 18:15:39 +0500

    Drill 8 again

--- day8_drill.py
+++ day8_drill.py
@@ -4,4 +4,37 @@ url = "https://api.github.com/repos/Suleman1533/Sakoonify_V2/contents/"
 response = requests.get(url)
 
 print(response.status_code)
-print(response.json())
+print(response.json()) 
+
+print("=" * 50)
+
+class User:
+    def __init__(self, user_name):
+        self.user_name = user_name
+
+
+# Session inherits from User
+class Session(User):
+    def __init__(self, user_name):
+        super().__init__(user_name)
+        self.history = []
+
+    def add_message(self, message):
+        self.history.append(message)
+
+
+# Dictionary storing Session objects
+sessions = {}
+
+sessions["101"] = Session("Suleman")
+sessions["102"] = Session("Ahmad")
+
+# Safe lookup
+session = sessions.get("101")
+
+if session:
+    session.add_message("Hello!")
+    print(session.user_name)
+    print(session.history)
+else:
+    print("Session not found.")
