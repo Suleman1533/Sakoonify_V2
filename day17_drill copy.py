1722daa1079fb115eb23432cb928382a272bbc7b
Suleman <muhammadsuleman1533@gmail.com>
2026-08-26 23:16:34 +0500

    Day 17 complete

--- day17_drill copy.py
+++ day17_drill copy.py
@@ -1,3 +1,4 @@
 preparing interview quesitgons for job roles of full stack developer as well ai engineer
 preparing interview quesitgons for job roles of full stack developer as well ai engineer
-looking for schorlarships for furhter study abroad
+looking for schorlarships for furhter study abroad 
+decided to starts iets from tomorrow
